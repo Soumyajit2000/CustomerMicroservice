6e159bee49fe40c13542aa8cc376f6eb9ea493bd
Soumyajit2000 <70477359+Soumyajit2000@users.noreply.github.com>
2022-09-01 18:28:15 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace CustomerMicroservice
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(options =>
-            options.WithOrigins("http://localhost:3000")
+            options.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader());
 
